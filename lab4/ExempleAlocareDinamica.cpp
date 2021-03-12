42b9f814ebd762d2ec0c6b7214f84331c149cab4
Alexandra Murgoci <alexa.murgoci@gmail.com>
2021-03-12 08:46:27 +0200

    lab 4 exemple alocare dinamica

---
+++ lab4/ExempleAlocareDinamica.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+
+class DynamicExample {
+    int m_size;
+    int* m_vector;
+
+public:
+    DynamicExample(int size, int initial_value)
+    {
+        m_size = size;
+        m_vector = new int[m_size];
+        for(int i=0;i<m_size;i++) m_vector[i]=initial_value;
+    }
+
+    DynamicExample(const DynamicExample &dynamicExample)
+    {
+        m_size = dynamicExample.m_size;
+        //ASA NU
+//        m_vector = dynamicExample.m_vector;
+        m_vector = new int[m_size];
+        for(int i=0;i<m_size;i++) m_vector[i] = dynamicExample.m_vector[i];
+    }
+
+    ~DynamicExample()
+    {
+        delete[] m_vector;
+    }
+
+    DynamicExample& operator=(const DynamicExample& dynamicExample)
+    {
+        m_size = dynamicExample.m_size;
+        //ASA NU
+//        m_vector = dynamicExample.m_vector;
+
+        //obiectul exista deja, deci trebuie sa eliberez memoria alocata inainte de a aloca din nou
+        delete[] m_vector;
+
+        m_vector = new int[m_size];
+        for(int i=0;i<m_size;i++) m_vector[i] = dynamicExample.m_vector[i];
+        return *this;
+    }
+
+    void Display()
+    {
+        for(int i=0;i<m_size;i++) std::cout << m_vector[i] << ' ';
+    }
+
+    bool ReplaceAt(int index, int value)
+    {
+        if(index >= m_size || index < 0) return false;
+        m_vector[index] = value;
+        return true;
+    }
+};
+
+void demoDynamicExample() {
+    DynamicExample d1(3, 0);
+    DynamicExample d2(d1);
+
+//    d2=d1;
+
+    d1.ReplaceAt(1, 4);
+
+    d1.Display(); // 0 4 0
+    std::cout << std::endl;
+    d2.Display(); // 0 0 0
+}
+
+class Node {
+    int m_info;
+    Node *m_next;
+
+public:
+    Node(int info)
+    {
+        m_info = info;
+        m_next = NULL;
+    }
+
+    void SetNext(Node* node)
+    {
+        m_next = node;
+    }
+
+    int GetInfo() {return m_info;}
+    void SetInfo(int info) {m_info = info;}
+
+    ~Node()
+    {
+        //NU vrem sa dezalocam aici memoria pentru un nod "exterior" obiectului meu
+//        delete m_next;
+    }
+};
+
+class ListExample {
+    Node *m_first;
+
+public:
+    ListExample(int info)
+    {
+        m_first = new Node(info);
+    }
+
+    //TODO
+    void Add(int info);
+    void Display();
+    ~ListExample()
+    {
+        //aici trebuie sa ne asiguram ca se elibereaza memoria pentru toate nodurile din lista
+    }
+};
+
+void demoListExample() {
+    //TODO
+    //creez o lista
+    //adaug 2 elemente
+}
+
+int main() {
+//    demoDynamicExample();
+    demoListExample();
+    return 0;
+}
