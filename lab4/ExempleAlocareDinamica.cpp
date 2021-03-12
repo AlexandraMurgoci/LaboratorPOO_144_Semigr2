d9334514346af971702adb6fe5405526fe5fc320
OvidiuIordache1 <ovidiu.iordache.2020@gmail.com>
2021-03-12 09:31:09 +0200

    lab4

--- lab4/ExempleAlocareDinamica.cpp
+++ lab4/ExempleAlocareDinamica.cpp
@@ -1,70 +1,53 @@
 #include <iostream>
 
-class DynamicExample {
+using namespace std;
+/*
+class DynamicExample
+{
     int m_size;
     int* m_vector;
 
 public:
-    DynamicExample(int size, int initial_value)
+    DynamicExample(int size, int init_value)
     {
         m_size = size;
         m_vector = new int[m_size];
-        for(int i=0;i<m_size;i++) m_vector[i]=initial_value;
+        for (int i=0;i<m_size;i++)
+            m_vector[i]=init_value;
     }
 
-    DynamicExample(const DynamicExample &dynamicExample)
+    DynamicExample(const DynamicExample & dynamicExample)
     {
         m_size = dynamicExample.m_size;
-        //ASA NU
-//        m_vector = dynamicExample.m_vector;
         m_vector = new int[m_size];
-        for(int i=0;i<m_size;i++) m_vector[i] = dynamicExample.m_vector[i];
+        for (int i=0;i<m_size;i++)
+            m_vector[i] = dynamicExample.m_vector[i];
     }
 
-    ~DynamicExample()
-    {
-        delete[] m_vector;
-    }
-
-    DynamicExample& operator=(const DynamicExample& dynamicExample)
+    DynamicExample & operator=(const DynamicExample & dynamicExample)
     {
         m_size = dynamicExample.m_size;
-        //ASA NU
-//        m_vector = dynamicExample.m_vector;
-
-        //obiectul exista deja, deci trebuie sa eliberez memoria alocata inainte de a aloca din nou
-        delete[] m_vector;
-
-        m_vector = new int[m_size];
-        for(int i=0;i<m_size;i++) m_vector[i] = dynamicExample.m_vector[i];
+        for (int i=0;i<m_size;i++)
+            m_vector[i] = dynamicExample.m_vector[i];
         return *this;
     }
 
-    void Display()
+    ~DynamicExample() {delete[] m_vector;}
+
+    void display
     {
-        for(int i=0;i<m_size;i++) std::cout << m_vector[i] << ' ';
+        for (int i=0;i<m_size;i++)
+            cout << m_vector[i] << ' ';
     }
 
-    bool ReplaceAt(int index, int value)
+    bool ReplaceAt (int index, int value)
     {
-        if(index >= m_size || index < 0) return false;
-        m_vector[index] = value;
-        return true;
+        if (index>= m_size || index<0) return false;
+            m_vector[index] = value;
+            return true;
     }
 };
-
-void demoDynamicExample() {
-    DynamicExample d1(3, 0);
-    DynamicExample d2(d1);
-
-//    d2=d1;
-
-    d1.ReplaceAt(1, 4);
-
-    d1.Display(); // 0 4 0
-    std::cout << std::endl;
-    d2.Display(); // 0 0 0
-}
+*/
 
 class Node {
     int m_info;
@@ -81,7 +64,7 @@ public:
     {
         m_next = node;
     }
-
+    Node* GetNext() {return m_next;}
     int GetInfo() {return m_info;}
     void SetInfo(int info) {m_info = info;}
 
@@ -94,26 +77,54 @@ public:
 
 class ListExample {
     Node *m_first;
+    Node *n;
 
 public:
     ListExample(int info)
     {
         m_first = new Node(info);
+        n = m_first;
     }
 
     //TODO
-    void Add(int info);
-    void Display();
+    void Add(int info)
+    {
+        Node* newnode = new Node(info);
+        n->SetNext(newnode);
+        n = newnode;
+
+    }
+    void Display()
+    {
+        Node* temp = m_first;
+        while (temp != NULL)
+        {
+            std::cout << temp->GetInfo() << ' ';
+            temp = temp->GetNext();
+        }
+    }
     ~ListExample()
     {
         //aici trebuie sa ne asiguram ca se elibereaza memoria pentru toate nodurile din lista
+
+        while (m_first !=NULL)
+        {
+            Node* temp = m_first;
+            m_first = m_first->GetNext();
+            delete temp;
+        }
+
     }
 };
 
 void demoListExample() {
     //TODO
     //creez o lista
+    ListExample lista1(3);
     //adaug 2 elemente
+    lista1.Add(5);
+    lista1.Add(6);
+    lista1.Display();
 }
 
 int main() {
