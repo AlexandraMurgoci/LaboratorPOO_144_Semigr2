4796d7bd7ac798ed4b2fdbca593b217520508220
Alexandra Murgoci <alexa.murgoci@gmail.com>
2021-04-02 09:30:27 +0300

    Bunicuta - after lab 7 - operator >>, functii pure, static (variabila, membru si metoda)

--- lab7/Bunicuta.cpp
+++ lab7/Bunicuta.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 class Forma
 {
+    static const double S_MAX_INALTIME;
 protected:
     double m_inaltime;
 public:
@@ -16,57 +17,76 @@ public:
     {
         std::cout << "Forma: inaltimea " << m_inaltime << std::endl;
     }
-    virtual double Volum() {return 0;}
+    virtual istream &Citire(istream& in) = 0;
+    virtual double Aria() = 0;
+    double Volum() {return m_inaltime * Aria();}
+    double Suprafata()
+    {
+        //TODO
+        return 0;
+    }
     double CantitateGem()
     {
         return Volum() * 2;
     }
     double CantitateFrisca()
     {
+        //GRESIT!: defapt cantitatea de frisca depinde de suprafata
+        //TODO: implementeaza cu suprafata
         return Volum();
     }
     virtual ~Forma(){}
+    static double GetMaxInaltime() {return S_MAX_INALTIME;}
 };
+
+const double Forma::S_MAX_INALTIME = 10;
 
 class Cerc: public Forma
 {
     double m_raza;
+    static int s_nr_cercuri;
 public:
-    Cerc(): Forma(), m_raza(0) {}
-    Cerc(double inaltime, double raza): Forma(inaltime), m_raza(raza) {}
+    Cerc(): Forma(), m_raza(0) {s_nr_cercuri++;}
+    Cerc(double inaltime, double raza): Forma(inaltime), m_raza(raza) {s_nr_cercuri++;}
+    Cerc(const Cerc& cerc): m_raza(cerc.m_raza), Forma(cerc.m_inaltime) {s_nr_cercuri++;}
+    ~Cerc() {s_nr_cercuri--;}
+    double GetRaza() {return m_raza;}
     void Afisare()
     {
-//        Forma::Afisare();
-//        std::cout << "Cerc: inaltime " << GetInaltime() << " raza: " << m_raza << std::endl;
         std::cout << "Cerc: inaltime " << m_inaltime << " raza: " << m_raza << std::endl;
     }
-    void Citire()
+    istream &Citire(istream& in)
     {
-        cin >> m_inaltime >> m_raza;
+        in >> m_inaltime >> m_raza;
+        if(m_inaltime > Forma::GetMaxInaltime()) cout << "Inaltime mai mare decat max<<endl";
+        return in;
     }
-    double Volum()
+    double Aria()
     {
-//        cout << "Fol volum din cerc!!" << endl;
-        return m_inaltime * 3.14 * m_raza * m_raza;
+        return 3.14 * m_raza * m_raza;
     }
+    static int GetNrCercuri() {return s_nr_cercuri;}
 };
+
+int Cerc::s_nr_cercuri = 0;
 
 class Dreptunghi: public Forma
 {
     double m_latura_mica;
     double m_latura_mare;
 public:
-    void Citire()
+    istream &Citire(istream& in)
     {
-        cin >> m_inaltime >> m_latura_mica >> m_latura_mare;
+        in >> m_inaltime >> m_latura_mica >> m_latura_mare;
+        return in;
     }
     void Afisare()
     {
         std::cout << "Dreptunghi: inaltime " << m_inaltime << " latura mica: " << m_latura_mica << " latura mare: " << m_latura_mare << std::endl;
     }
-    double Volum()
+    double Aria()
     {
-        return m_inaltime * m_latura_mica * m_latura_mare;
+        return m_latura_mica * m_latura_mare;
     }
 };
 
@@ -74,17 +94,19 @@ class Patrat: public Forma
 {
     double m_latura;
 public:
-    void Citire()
+    double GetLatura() {return m_latura;}
+    istream &Citire(istream& in)
     {
-        cin >> m_inaltime >> m_latura;
+        in >> m_inaltime >> m_latura;
+        return in;
     }
     void Afisare()
     {
         std::cout << "Patrat: inaltime " << m_inaltime << " latura: " << m_latura << std::endl;
     }
-    double Volum()
+    double Aria()
     {
-        return m_inaltime * m_latura;
+        return m_latura * m_latura;
     }
 };
 
@@ -93,19 +115,24 @@ class Triunghi: public Forma
     double m_cateta_mica;
     double m_cateta_mare;
 public:
-    void Citire()
+    istream &Citire(istream& in)
     {
-        cin >> m_inaltime >> m_cateta_mica >> m_cateta_mare;
+        in >> m_inaltime >> m_cateta_mica >> m_cateta_mare;
+        return in;
     }
     void Afisare()
     {
         std::cout << "Triunghi: inaltime " << m_inaltime << " cateta mica: " << m_cateta_mica << " cateta mare: " << m_cateta_mare << std::endl;
     }
-    double Volum()
+    double Aria()
     {
-        return m_inaltime * m_cateta_mica * m_cateta_mare;
+        return m_cateta_mica * m_cateta_mare;
     }
 };
+
+istream& operator>>(std::istream& in, Forma& forma) {
+    return forma.Citire(in);
+}
 
 void demo()
 {
@@ -115,11 +142,11 @@ void demo()
 //    Cerc cerc(5,2);
 //    cerc.Afisare();
 
-    Forma *formaGenerica2 = new Forma(4);
-    formaGenerica2 -> Afisare();
+//    Forma *formaGenerica2 = new Forma(4);
+//    formaGenerica2 -> Afisare();
 
-    Forma *cerc2 = new Cerc(3,7);
-    cerc2 -> Afisare();
+//    Forma *cerc2 = new Cerc(3,7);
+//    cerc2 -> Afisare();
 
 //    Cerc cerc3(5, 3);
 //    Forma& forma = cerc3;
@@ -141,22 +168,22 @@ Forma * citireForma()
     cin >> tipForma;
     if(tipForma == 1) {
         Cerc *cerc = new Cerc();
-        cerc -> Citire();
+        cerc -> Citire(cin);
         forma = cerc;
     }
     else if(tipForma == 2) {
         Dreptunghi *dreptunghi = new Dreptunghi();
-        dreptunghi -> Citire();
+        dreptunghi -> Citire(cin);
         forma = dreptunghi;
     }
     else if(tipForma == 3) {
         Patrat *patrat = new Patrat();
-        patrat -> Citire();
+        patrat -> Citire(cin);
         forma = patrat;
     }
     else if(tipForma == 4) {
         Triunghi *triunghi = new Triunghi();
-        triunghi -> Citire();
+        triunghi -> Citire(cin);
         forma = triunghi;
     }
     return forma;
@@ -177,14 +204,13 @@ vector<Forma *> citimCeleNFormeInitiale()
     return forme;
 }
 
-int main() {
-//    demo();
-
+void MeniuInteractiv()
+{
     vector<Forma *> formeleBunicutei = citimCeleNFormeInitiale();
     for(int i=0; i<formeleBunicutei.size(); i++) formeleBunicutei[i] -> Afisare();
 
     int comanda = 0;
-    int comanda_stop = 5;
+    int comanda_stop = 6;
 
     while(comanda != comanda_stop)
     {
@@ -218,9 +244,60 @@ int main() {
         else if(comanda == 4) {
             for(int i=0; i<formeleBunicutei.size(); i++) formeleBunicutei[i] -> Afisare();
         }
+        else if(comanda == 5) {
+            cout << Cerc::GetNrCercuri() << endl;
+        }
     }
 
     for(int i=0; i<formeleBunicutei.size(); i++) delete formeleBunicutei[i];
+}
+
+void demo1()
+{
+    Forma *forma = new Cerc(1,2);
+    //vreau forma -> GetRaza()
+
+    if(Cerc *cerc = dynamic_cast<Cerc *>(forma))
+    {
+        cout << cerc -> GetRaza();
+    }
+
+    if(Patrat *patrat = dynamic_cast<Patrat *>(forma))
+    {
+        cout << patrat -> GetLatura();
+    }
+}
+
+void demo2(){
+    Cerc cerc;
+    cin >> cerc;
+    cerc.Afisare();
+    cout << endl;
+
+    Dreptunghi dreptunghi;
+    cin >> dreptunghi;
+    dreptunghi.Afisare();
+}
+
+class DemoStaticVariable
+{
+public:
+    DemoStaticVariable() {cout << "CI" << endl;}
+    ~DemoStaticVariable() {cout << "D" << endl;}
+};
+
+int main() {
+//    demo();
+//    MeniuInteractiv();
+//    demo1();
+//    demo2();
+
+    if(0 == 0)
+    {
+        static DemoStaticVariable obj;
+    }
+
+    cout << "Finalul programului" << endl;
 
     return 0;
 }
